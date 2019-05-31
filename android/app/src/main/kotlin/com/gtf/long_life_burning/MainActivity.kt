d25111ffc4ff5bd8d6a557fcd20de87a31369a3e
sukawit_c <skw.crd.05@gmail.com>
2019-05-31 19:24:05 +0700

    initial project

---
+++ android/app/src/main/kotlin/com/gtf/long_life_burning/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.gtf.long_life_burning
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
