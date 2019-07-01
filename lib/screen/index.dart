0fecc518a29c3cd1ae09691b94fc8461ca177bb1
skwcrd <skw.crd.05@gmail.com>
2019-07-01 10:39:08 +0700

    delete navBar file and update index page

--- lib/screen/index.dart
+++ lib/screen/index.dart
@@ -3,8 +3,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:long_life_burning/constants/icons.dart';
 import 'package:long_life_burning/constants/platform.dart';
 import 'package:long_life_burning/routes/route.dart';
-import 'package:long_life_burning/widgets/nav.dart';
-import 'package:long_life_burning/widgets/scaffold.dart';
 
 class Index extends StatefulWidget {
   @override
@@ -27,42 +25,65 @@ class _IndexState extends State<Index> {
   @override
   Widget build(BuildContext context) {
 
-    Widget navBar;
-
     if ( Platforms.isIOS ) {
-      navBar = IOSNavBar(
-        index: _pageIndex,
-        callback: onChanged,
-        items: [
-          navBarItem('', IconsiOS.home),
-          navBarItem('', IconsiOS.near_me),
-          navBarItem('', IconsiOS.marathon),
-          navBarItem('', IconsiOS.group),
-          navBarItem('', IconsiOS.others),
-        ],
-      );
-
-      return IOSScaffold(
-        body: PageStorage(child: currentPage, bucket: bucket),
-        navBar: navBar,
+      return CupertinoTabScaffold(
+        resizeToAvoidBottomInset: true,
+        tabBar: CupertinoTabBar(
+          currentIndex: _pageIndex,
+          onTap: onChanged,
+          items: [
+            navBarItem('', IconsiOS.home),
+            navBarItem('', IconsiOS.near_me),
+            navBarItem('', IconsiOS.marathon),
+            navBarItem('', IconsiOS.group),
+            navBarItem('', IconsiOS.others),
+          ],
+          activeColor: CupertinoColors.activeBlue,
+          inactiveColor: CupertinoColors.inactiveGray,
+          backgroundColor: CupertinoColors.lightBackgroundGray,
+          iconSize: 30.0,
+          border: Border(
+            top: BorderSide(
+              color: Color(0x4C000000),
+              width: 0.0, // One physical pixel.
+              style: BorderStyle.solid,
+            ),
+          ),
+        ),
+        tabBuilder: (BuildContext context, int index) {
+          return CupertinoPageScaffold(
+            child: PageStorage(child: currentPage, bucket: bucket),
+            resizeToAvoidBottomInset: true,
+          );
+        },
       );
     }
 
-    navBar = AndroidNavBar(
-      index: _pageIndex,
-      callback: onChanged,
-      items: [
-        navBarItem('', IconsAndroid.home),
-        navBarItem('', IconsAndroid.near_me),
-        navBarItem('', IconsAndroid.marathon),
-        navBarItem('', IconsAndroid.group),
-        navBarItem('', IconsAndroid.others),
-      ],
-    );
-
-    return AndroidScaffold(
+    return Scaffold(
+      resizeToAvoidBottomPadding: true,
       body: PageStorage(child: currentPage, bucket: bucket),
-      navBar: navBar,
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.white,
+        clipBehavior: Clip.none,
+        child: BottomNavigationBar(
+          items: [
+            navBarItem('', IconsAndroid.home),
+            navBarItem('', IconsAndroid.near_me),
+            navBarItem('', IconsAndroid.marathon),
+            navBarItem('', IconsAndroid.group),
+            navBarItem('', IconsAndroid.others),
+          ],
+          currentIndex: _pageIndex,
+          onTap: onChanged,
+          iconSize: 28.0,
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.white,
+          selectedItemColor: Colors.blue,
+          unselectedItemColor: Colors.black54,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+        ),
+      ),
     );
 
   }
