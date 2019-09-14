a909f46d7e7ef00b44f8bac00b685f13992353f8
Nuntawat_n <got_fc_got6@hotmail.com>
2019-09-14 16:49:41 +0700

    Update signup in cloud firebase.

--- lib/screen/login/signup.dart
+++ lib/screen/login/signup.dart
@@ -7,7 +7,6 @@ SingingCharacter _character = SingingCharacter.male;
 @immutable
 class SignUpPage extends StatefulWidget {
 
-
   final VoidCallback signin;
   
   SignUpPage({
@@ -19,33 +18,34 @@ class SignUpPage extends StatefulWidget {
   _SignUpPageState createState() => _SignUpPageState();
 }
 
-// class Reply {
-//     Reply(this.replyHight, this.replyWight);
-//       final String replyHight;
-//       final String replyWight;
+class Reply {
+    Reply(this.replyHeight, this.replyWight);
+      final String replyHeight;
+      final String replyWight;
     
 
-//       String getName() {
-//         return replyHight;
-//       }
+      String getName() {
+        return replyHeight;
+      }
 
-//       String getText() {
-//         return replyWight;
-//       }
+      String getText() {
+        return replyWight;
+      }
 
-//     }
+    }
 
 class _SignUpPageState extends State<SignUpPage> {
 
+  DateTime date = DateTime(2000, 1, 1);
   GlobalKey<FormState> fromKey;
   TextEditingController emailController;
   TextEditingController passwordController;
   TextEditingController confirmController;
-  // TextEditingController hightController = TextEditingController();
-  // TextEditingController weightController = TextEditingController();
-
-  String _email;
-  String _password;
+  TextEditingController heightController;
+  TextEditingController weightController;
+  TextEditingController birthController;
+  TextEditingController genderController;
+  final db = Firestore.instance;
 
   @override
   void initState() { 
@@ -54,6 +54,9 @@ class _SignUpPageState extends State<SignUpPage> {
     emailController = TextEditingController();
     passwordController = TextEditingController();
     confirmController = TextEditingController();
+    heightController = TextEditingController();
+    weightController = TextEditingController();
+    genderController= TextEditingController();
   }
 
   @override
@@ -64,6 +67,9 @@ class _SignUpPageState extends State<SignUpPage> {
     emailController.dispose();
     passwordController.dispose();
     confirmController.dispose();
+    heightController.dispose();
+    weightController.dispose();
+    genderController.dispose();
     super.dispose();
   }
 
@@ -77,35 +83,40 @@ class _SignUpPageState extends State<SignUpPage> {
     }
   }
 
-  void validateAndSubmit() {
-    if(validateAndSave()){
+  Future<void> signUp() async {
+    if (validateAndSave()) {
       try {
-        UserOptions.auth.createUserWithEmailAndPassword(email: _email, password: _password).then((result) {
-          if (result != null && result.user != null) {
-            widget.signin();
-          }
-        });
+        String email = emailController.text.trim();
+        String password = passwordController.text.trim();
+        String confirmPassword = confirmController.text.trim();
+        String gender;
+        num height = num.parse(heightController.text.trim());
+        num weight = num.parse(weightController.text.trim());
+        if(_character == SingingCharacter.male ){
+          gender = 'male'; 
+        }else if (_character == SingingCharacter.female){
+         gender = 'female'; 
+        }
+        if (password == confirmPassword && password.length >= 8) {
+          await UserOptions.auth.createUserWithEmailAndPassword(email: email, password: password).then((result) async {
+            if (result != null && result.user != null) {
+              await db.collection('users').add({
+                'uid': result.user.uid,
+                'name': 'Anonymus',
+                'height': height,
+                'weight': weight,
+                'dateOfBirth': date,
+                'gender' : gender,
+              }).then((ref) => print("$ref"));
+              widget.signin();
+            }
+          });
+        } else {
+          print("Password and Confirm-password is not match.");
+        }
       } catch (e) {
         print('Error : $e');
       }
-    }
-  }
-
-  void signUp() {
-    String email = emailController.text.trim();
-    String password = passwordController.text.trim();
-    String confirmPassword = confirmController.text.trim();
-    if (password == confirmPassword && password.length >= 8) {
-      UserOptions.auth
-        .createUserWithEmailAndPassword(email: email, password: password)
-        .then((user) {
-          print("Sign up user successful.");
-        })
-        .catchError((e) {
-          print(e.message);
-        });
-    } else {
-      print("Password and Confirm-password is not match.");
     }
   }
 
@@ -164,7 +175,7 @@ class _SignUpPageState extends State<SignUpPage> {
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[                      
+                children: <Widget>[
                   buildTextFieldEmail(),
                   buildTextFieldPassword(),
                   buildTextFieldConfirmPassword(),
@@ -173,11 +184,13 @@ class _SignUpPageState extends State<SignUpPage> {
                       mainAxisAlignment: MainAxisAlignment.spaceAround,
                       mainAxisSize: MainAxisSize.max,
                       children: <Widget>[
-                        // buildTextFieldHight(),
-                        // buildTextFieldWeight(),
+                        buildTextFieldHeight(),
+                        buildTextFieldWeight(),
                       ],
                     ),
                   ),
+                  buildTextFieldBirth(),
+              
                   ListTile(
                     title: Text(
                       'Male',
@@ -205,7 +218,7 @@ class _SignUpPageState extends State<SignUpPage> {
                         });
                       }
                     ),
-                  ),                
+                  ),
                   buildButtonSignup(),
                 ],
               ),
@@ -225,7 +238,6 @@ class _SignUpPageState extends State<SignUpPage> {
           controller: emailController,
             decoration: InputDecoration.collapsed(hintText: "Email"),
             validator: (value) => value.isEmpty ? 'Email can\'t be empty' : null,
-            onSaved: (value) =>_email =value,
             style: TextStyle(fontSize: 18)
               )
             );
@@ -244,7 +256,6 @@ class _SignUpPageState extends State<SignUpPage> {
         obscureText: true,
         decoration: InputDecoration.collapsed(hintText: "Password"),
         validator: (value) => value.isEmpty ? 'Password can\'t be empty' : null,
-        onSaved: (value) =>_password =value,
         style: TextStyle(
           fontSize: 18,
         ),
@@ -272,56 +283,167 @@ class _SignUpPageState extends State<SignUpPage> {
     );
   }
 
-// Widget buildTextFieldHight() {
-//     return Container(
-//       width: 150,
-//       padding: EdgeInsets.all(12),
-//       margin: EdgeInsets.only(top: 12),
-//       decoration: BoxDecoration(
-//         color: Colors.grey[50],
-//         borderRadius: BorderRadius.circular(16),
-//       ),
-//       child: TextFormField(
-//         controller: hightController,
-//         decoration: InputDecoration.collapsed(hintText: "Hight"),
-//         validator: (value) => value.isEmpty ? 'Hight can\'t be empty' : null,
-//         style: TextStyle(
-//           fontSize: 18,
-//         ),
-//       )
-//     );
-//   }
+Widget buildTextFieldHeight() {
+    return Container(
+      width: 150,
+      padding: EdgeInsets.all(12),
+      margin: EdgeInsets.only(top: 12),
+      decoration: BoxDecoration(
+        color: Colors.grey[50],
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: TextFormField(
+        keyboardType: TextInputType.number,
+        controller: heightController,
+        decoration: InputDecoration.collapsed(hintText: "Height"),
+        validator: (value) => value.isEmpty ? 'Height can\'t be empty' : null,
+        style: TextStyle(
+          fontSize: 18,
+        ),
+      )
+    );
+  }
 
   
-//   Widget buildTextFieldWeight() {
-//     return Container(
-//       width: 150,
-//       padding: EdgeInsets.all(12),
-//       margin: EdgeInsets.only(top: 12),
-//       decoration: BoxDecoration(
-//         color: Colors.grey[50],
-//         borderRadius: BorderRadius.circular(16),
-//       ),
-//       child: TextFormField(
-//        controller: weightController,
-//         decoration: InputDecoration.collapsed(hintText: "Weight"),
-//         validator: (value) => value.isEmpty ? 'Weight can\'t be empty' : null,
-//         style: TextStyle(
-//           fontSize: 18,
-//         ),
-//       )
-//     );
-//   }
+  Widget buildTextFieldWeight() {
+    return Container(
+      width: 150,
+      padding: EdgeInsets.all(12),
+      margin: EdgeInsets.only(top: 12),
+      decoration: BoxDecoration(
+        color: Colors.grey[50],
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: TextFormField(
+        keyboardType: TextInputType.number,
+       controller: weightController,
+        decoration: InputDecoration.collapsed(hintText: "Weight"),
+        validator: (value) => value.isEmpty ? 'Weight can\'t be empty' : null,
+        style: TextStyle(
+          fontSize: 18,
+        ),
+      )
+    );
+  }
 
+   Widget buildTextFieldBirth() {
+    return Container(
+       padding: EdgeInsets.all(12),
+       margin: EdgeInsets.only(top: 12),
+        decoration: BoxDecoration(
+            color: Colors.grey[50], borderRadius: BorderRadius.circular(16)),
+      child: GestureDetector(
+        onTap: () async => await showCupertinoModalPopup<void>(
+          context: context,
+          builder: (BuildContext context) => _buildBottomPicker(
+            CupertinoDatePicker(
+              mode: CupertinoDatePickerMode.date,
+              initialDateTime: date,
+              maximumYear: DateTime.now().year - 1,
+              onDateTimeChanged: (DateTime t) {
+                setState(() {
+                  date = t;
+                });
+              },
+            ),
+            title: 'Date of Birth',
+            context: context,
+          ),
+        ),
+        child: Container(
+          alignment: AlignmentDirectional.center,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            child: SafeArea(
+              child: Text(
+                DateFormat.yMMMMd().format(date),
+                style: TextStyle(
+                  fontSize: 18,
+                  color: CupertinoColors.inactiveGray,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
   
   RaisedButton buildButtonSignup() {
     return RaisedButton(
         child: Text("sign up",
             textAlign: TextAlign.center,
             style: TextStyle(fontSize: 18, color: Colors.black)),
-             onPressed:validateAndSubmit,
+             onPressed: () async => await signUp(),
             padding: EdgeInsets.all(12)
   );           
+  }
+
+  Widget _buildBottomPicker(Widget picker, {String title, BuildContext context}) {
+    return Container(
+      height: SizeConfig.setHeight(268.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(18.0),
+          topRight: Radius.circular(18.0),
+        ),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Expanded(
+            flex: 2,
+            child: DefaultTextStyle(
+              style: TextStyle(
+                color: CupertinoColors.inactiveGray,
+                fontSize: 18.0,
+              ),
+              child: Container(
+                alignment: Alignment.topCenter,
+                padding: EdgeInsets.only(
+                  top: 18.0,
+                  left: 24.0,
+                  right: 24.0,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(title),
+                    GestureDetector(
+                      onTap: () => Navigator.of(context).pop(),
+                      child: Container(
+                        child: Text(
+                          'Done',
+                          style: TextStyle(
+                            color: CupertinoColors.activeBlue,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 8,
+            child: DefaultTextStyle(
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 22.0,
+              ),
+              child: GestureDetector(
+                onTap: () { },
+                child: SafeArea(
+                  child: picker,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
          
