1a76386d851fd1ca6888316e6f85f59f16ce6e3b
SkWcRd <32414976+skwcrd@users.noreply.github.com>
2019-11-20 11:20:50 +0700

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
-const serviceAccount = require("./adminsdk.json");
-
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://long-life-burnning.firebaseio.com"
